a4169759eecfb74f34905847849ce71103ddcf70
LifeH <hwie.zhu@gmail.com>
2019-09-16 11:24:59 -0400

    migrated db, and added secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_155850) do
+ActiveRecord::Schema.define(version: 2019_09_15_170418) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.string "last_name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
