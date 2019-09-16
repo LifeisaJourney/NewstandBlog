1fde6ef411ff7b3172dea7efb0b1ccde6cea22bd
LifeH <hwie.zhu@gmail.com>
2019-09-16 14:47:49 -0400

    fixed user_params and signup form for :password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            redirect_to '/'
+        else
+            redirect_to '/signup'
+        end
+    end
 
     private
         def user_params
