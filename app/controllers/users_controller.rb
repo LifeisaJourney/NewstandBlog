7f336a4137d641ce0df0ff0fa86f3257cdbecf74
LifeH <hwie.zhu@gmail.com>
2019-09-16 14:32:12 -0400

    added user private params, paths, and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:first_name, :last_name, :email, :password)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :articles
+  resources :articles 
   root 'articles#index'
-  
+  get '/signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
