e67d5476ba8bac786658e36fc901626c4ee5e4fc
LifeH <hwie.zhu@gmail.com>
2019-09-16 16:46:35 -0400

    added routes for session controller and new action

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   get '/signup' => 'users#new'
   resources :users
+  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
