3a4721783504d7d4620d5b6c5d255e692644d238
LifeH <hwie.zhu@gmail.com>
2019-09-17 11:48:46 -0400

    sessions controller, views, routes, and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   resources :users
   get '/login' => 'sessions#new'
-  get '/login' => 'sessions#create'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
